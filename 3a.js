8c36185611fe8b0b47bb6e1a756a9b84b73ab64e
Murilo <murilo.fonseca.araujo@escola.pr.gov.br>
2025-09-01 11:23:01 -0300

    atividade 1 a 3

---
+++ 3a.js
@@ -0,0 +1,10 @@
+let nota = 75
+if (nota >= 60){
+    console.log("b")
+}
+else if (nota > 80){
+    console.log("a")
+}
+else {
+    console.log("c")
+}
---
+++ 3b.js
@@ -0,0 +1,10 @@
+let temperatura = 22
+if (temperatura >= 22){
+    console.log("agradavel")
+}
+else if (temperatura >= 26){
+    console.log("quente")
+}
+else {
+    console.log("frio")
+}
